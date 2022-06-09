92938f8a2d92b51bf8c94e1730e5f4df147d2966
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-09 23:46:15 +0200

    Bringing up the size

--- Travner/GuideHeaderView.swift
+++ Travner/GuideHeaderView.swift
@@ -22,8 +22,10 @@ struct GuideHeaderView: View {
 
             NavigationLink(destination: EmptyView()) {
                 Image(systemName: "square.and.pencil")
+                    .imageScale(.large)
             }
         }
+        .padding(.bottom, 10)
     }
 }
 
