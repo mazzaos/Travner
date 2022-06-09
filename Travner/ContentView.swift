97cbcbab4635035df0420f361e10115aca451891
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-09 20:58:34 +0200

    Introducing a Tabview

--- Travner/ContentView.swift
+++ Travner/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "map.fill")
+                    Text("Guides")
+                }
+
+            GuidesView(showClosedGuides: false)
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Open")
+                }
+
+            GuidesView(showClosedGuides: true)
+                .tabItem {
+                    Image(systemName: "checkmark")
+                    Text("Closed")
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+
     static var previews: some View {
         ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
     }
 }
