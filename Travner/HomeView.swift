cb1d9b2e2366b23d5652e06f23ce08232151de19
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-09 20:45:56 +0200

    Starting with Home

---
+++ Travner/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  Travner
+//
+//  Created by Lorenzo Lins Mazzarotto on 09/06/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("My Guides")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
