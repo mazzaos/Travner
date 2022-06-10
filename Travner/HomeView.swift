58904d75d9e5e9fc44cf8621c02292b1b3e21d01
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-10 11:47:40 +0200

    Adding upcoming places

--- Travner/HomeView.swift
+++ Travner/HomeView.swift
@@ -16,6 +16,10 @@ struct HomeView: View {
     @FetchRequest(entity: Guide.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Guide.title, ascending: true)], predicate: NSPredicate(format: "closed = false")) var guides: FetchedResults<Guide>
     let places: FetchRequest<Place>
 
+    var guideRows: [GridItem] {
+        [GridItem(.fixed(100))]
+    }
+
     init() {
         let request: NSFetchRequest<Place> = Place.fetchRequest()
         request.predicate = NSPredicate(format: "completed = false")
@@ -26,10 +30,6 @@ struct HomeView: View {
 
         request.fetchLimit = 10
         places = FetchRequest(fetchRequest: request)
-    }
-
-    var guideRows: [GridItem] {
-        [GridItem(.fixed(100))]
     }
 
     var body: some View {
@@ -57,11 +57,55 @@ struct HomeView: View {
                             }
                         }
                         .padding([.horizontal, .top])
+                        .fixedSize(horizontal: false, vertical: true)
                     }
+
+                    VStack(alignment: .leading) {
+                        list("Up next", for: places.wrappedValue.prefix(3))
+                        list("More to explore", for: places.wrappedValue.dropFirst(3))
+                    }
+                    .padding(.horizontal)
                 }
             }
             .background(Color.systemGroupedBackground.ignoresSafeArea())
             .navigationTitle("My Guides")
+        }
+    }
+
+    @ViewBuilder func list(_ title: String, for places: FetchedResults<Place>.SubSequence) -> some View {
+        if places.isEmpty {
+            EmptyView()
+        } else {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.secondary)
+                .padding(.top)
+
+            ForEach(places) { place in
+                NavigationLink(destination: EditPlaceView(place: place)) {
+                    HStack(spacing: 20) {
+                        Circle()
+                            .stroke(Color(place.guide?.guideColor ?? "Light Blue"), lineWidth: 3)
+                            .frame(width: 44, height: 44)
+
+                        VStack(alignment: .leading) {
+                            Text(place.placeName)
+                                .font(.title2)
+                                .foregroundColor(.primary)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+
+                            if place.placeDetail.isEmpty == false {
+                                Text(place.placeDetail)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                    .padding()
+                    .background(Color.secondarySystemGroupedBackground)
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.2), radius: 5)
+                }
+            }
         }
     }
 }
