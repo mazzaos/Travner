189e3b981382562ac47516d1668b4533c6013b72
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-10 11:39:17 +0200

    Multiple fetch requests

--- Travner/HomeView.swift
+++ Travner/HomeView.swift
@@ -5,12 +5,28 @@
 //  Created by Lorenzo Lins Mazzarotto on 09/06/22.
 //
 
+import CoreData
 import SwiftUI
 
 struct HomeView: View {
     static let tag: String? = "Guides"
     
     @EnvironmentObject var dataController: DataController
+
+    @FetchRequest(entity: Guide.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Guide.title, ascending: true)], predicate: NSPredicate(format: "closed = false")) var guides: FetchedResults<Guide>
+    let places: FetchRequest<Place>
+
+    init() {
+        let request: NSFetchRequest<Place> = Place.fetchRequest()
+        request.predicate = NSPredicate(format: "completed = false")
+
+        request.sortDescriptors = [
+            NSSortDescriptor(keyPath: \Place.priority, ascending: false)
+        ]
+
+        request.fetchLimit = 10
+        places = FetchRequest(fetchRequest: request)
+    }
 
     var body: some View {
         NavigationView {
