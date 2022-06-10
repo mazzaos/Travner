7bceef2d7f6264554fa4257dd4687c9e7c142155
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-10 11:37:50 +0200

    Building something custom

---
+++ Travner/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  Travner
+//
+//  Created by Lorenzo Lins Mazzarotto on 10/06/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
