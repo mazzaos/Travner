c89d49dceb7d532e711be3593091c9309e5779e5
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-07-07 11:09:18 +0200

    An easy one: sorting

---
+++ TravnerTests/ExtensionTests.swift
@@ -0,0 +1,34 @@
+//
+//  ExtensionTests.swift
+//  TravnerTests
+//
+//  Created by Lorenzo Lins Mazzarotto on 07/07/22.
+//
+
+import XCTest
+@testable import Travner
+
+class ExtensionTests: XCTestCase {
+    func testSequenceKeyPathSortingSelf() {
+        let items = [1, 4, 3, 2, 5]
+        let sortedItems = items.sorted(by: \.self)
+        XCTAssertEqual(sortedItems, [1, 2, 3, 4, 5], "The sorted numbers must be ascending.")
+    }
+
+    func testSequenceKeyPathSortingCustom() {
+        struct Example: Equatable {
+            let value: String
+        }
+
+        let example1 = Example(value: "a")
+        let example2 = Example(value: "b")
+        let example3 = Example(value: "c")
+        let array = [example1, example2, example3]
+
+        let sortedItems = array.sorted(by: \.value) {
+            $0 > $1
+        }
+
+        XCTAssertEqual(sortedItems, [example3, example2, example1], "Reverse sorting should yield c, b, a.")
+    }
+}
