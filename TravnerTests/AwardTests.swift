81cf9ebd278db2fd9b8412dab8b66a83418181f1
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-07-07 03:00:42 +0200

    Counting complex awards

--- TravnerTests/AwardTests.swift
+++ TravnerTests/AwardTests.swift
@@ -23,4 +23,51 @@ class AwardTests: BaseTestCase {
             XCTAssertFalse(dataController.hasEarned(award: award), "New users should have no earned awards")
         }
     }
+
+    func testPlaceAwards() throws {
+        let values = [1, 10, 20, 50, 100, 250, 500, 1000]
+
+        for (count, value) in values.enumerated() {
+            var places = [Place]()
+
+            for _ in 0..<value {
+                let place = Place(context: managedObjectContext)
+                places.append(place)
+            }
+
+            let matches = awards.filter { award in
+                award.criterion == "places" && dataController.hasEarned(award: award)
+            }
+
+            XCTAssertEqual(matches.count, count + 1, "Adding \(value) places should unlock \(count + 1) awards.")
+
+            for place in places {
+                dataController.delete(place)
+            }
+        }
+    }
+
+    func testCompletedAwards() throws {
+        let values = [1, 10, 20, 50, 100, 250, 500, 1000]
+
+        for (count, value) in values.enumerated() {
+            var places = [Place]()
+
+            for _ in 0..<value {
+                let place = Place(context: managedObjectContext)
+                place.completed = true
+                places.append(place)
+            }
+
+            let matches = awards.filter { award in
+                award.criterion == "complete" && dataController.hasEarned(award: award)
+            }
+
+            XCTAssertEqual(matches.count, count + 1, "Completing \(value) places should unlock \(count + 1) awards.")
+
+            for place in places {
+                dataController.delete(place)
+            }
+        }
+    }
 }
